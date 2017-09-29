1243b8b1938e9d68a86995c73c5d2dec52a64e8d
Aurélien Labrosse <aurelien@pollen-metrology.com>
2017-09-30 01:54:45 +0200

    Migrate project to AVR Studio 6

---
+++ ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 30/09/2017 00:12:45
+ * Author : aurelien
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ qp_nano_avr/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * qp_nano_avr.cpp
+ *
+ * Created: 15/08/2017 15:20:25
+ * Author : aurelien
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
